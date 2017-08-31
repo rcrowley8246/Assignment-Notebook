d3679099a718efb23c594724c72ada79b56012b5
apcs2 <apcs2@p-162mba-07.d214.org>
2017-08-31 09:34:11 -0500

    required tableView functions added.

--- assignmentNotebook/ViewController.swift
+++ assignmentNotebook/ViewController.swift
@@ -1,25 +1,36 @@
-//
-//  ViewController.swift
-//  assignmentNotebook
-//
-//  Created by apcs2 on 8/30/17.
-//  Copyright © 2017 apcs2. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var homeworkArray = ["", "", "", ""]
+    
+    
+    override func viewDidLoad()
+    
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+    return cell
+    
     }
-
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return homeworkArray.count
+    }
 
 }
 
